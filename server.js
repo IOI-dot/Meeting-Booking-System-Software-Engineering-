1efdebeb09d82be8a3a636ea7d3e6fd6b1ae0c01
Omar <omarelhussein@aucegypt.edu>
2026-04-12 12:27:46 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/rooms', (req, res) => {
+    const fakeRooms = [
+        { id: 1, room_name: "101", capacity: 4, technology: "TV" },
+        { id: 2, room_name: "102", capacity: 10, technology: "Projector" }
+    ];
+    res.json(fakeRooms);
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is vibing and running on http://localhost:${PORT}`);
+});
