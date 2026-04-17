3f26e314e14cd87b35c40d6c1c586948359654db
Omar <omarelhussein@aucegypt.edu>
2026-04-17 18:53:00 +0200

    Update Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,13 +1,34 @@
+// models/Booking.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Booking = sequelize.define('Booking', {
-    bookingID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    userID: { type: DataTypes.INTEGER, allowNull: false },
-    roomID: { type: DataTypes.INTEGER, allowNull: false },
-    startTime: { type: DataTypes.STRING, allowNull: false },
+    bookingID: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true,
+        field: 'booking_id' // Forces the DB column to be lowercase
+    },
+    userID: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+        field: 'user_id'
+    },
+    roomID: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+        field: 'room_id'
+    },
+    startTime: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        field: 'start_time'
+    },
     date: { type: DataTypes.DATEONLY, allowNull: false },
     status: { type: DataTypes.STRING, defaultValue: 'Confirmed' }
+}, {
+    tableName: 'bookings',
+    timestamps: false
 });
 
 module.exports = Booking;
